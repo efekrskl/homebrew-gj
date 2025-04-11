ccae7a9d84370226b55e3b8f1a161a66f2935d8d
Efe Karasakal <dogukankrskl@gmail.com>
2025-04-11 13:46:41 +0200

    fix: correct url

--- Formula/gj.rb
+++ Formula/gj.rb
@@ -1,7 +1,7 @@
 class Gj < Formula
   desc "Dead simple CLI for journaling"
-  homepage "https://github.com/yourname/gj"
-  url "https://github.com/yourname/gj/releases/download/v0.1.0/gj-macos.zip"
+  homepage "https://github.com/efekrskl/gj"
+  url "https://github.com/efekrskl/gj/releases/download/v0.1.0/gj-macos.zip"
   sha256 "b36992c1ad94ba26fd95b9fc74d33cbb68e1ae68e2b601c5c189e4f3cb94e13d"
   version "0.1.0"
 
