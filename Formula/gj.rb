8188b1ae473ddf09b4bf8d0ab343cd7ff1aa48bc
Efe Karasakal <dogukankrskl@gmail.com>
2025-04-14 23:07:48 +0200

    feat: update to v0.2.0

--- Formula/gj.rb
+++ Formula/gj.rb
@@ -1,9 +1,9 @@
 class Gj < Formula
   desc "Dead simple CLI for journaling"
   homepage "https://github.com/efekrskl/gj"
-  url "https://github.com/efekrskl/gj/releases/download/v0.1.0/gj-macos.zip"
-  sha256 "b36992c1ad94ba26fd95b9fc74d33cbb68e1ae68e2b601c5c189e4f3cb94e13d"
-  version "0.1.0"
+  url "https://github.com/efekrskl/gj/releases/download/v0.2.0/gj-macos.zip"
+  sha256 "6981ec114d5960d9d682ab2590b2f2834f87355206801113e12a745d5629d23f"
+  version "0.2.0"
 
   def install
     bin.install "gj"
